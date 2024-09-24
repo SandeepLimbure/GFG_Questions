e14cc1f3ba1d2fb6330d864e9ea65aa318f798ba
sandeep limbure <127886236+SandeepLimbure@users.noreply.github.com>
2024-09-24 21:30:29 +0530

    Add files via upload

---
+++ Array_Basic/CountGreaterThanX.java
@@ -0,0 +1,18 @@
+package Array_Basic;
+
+public class CountGreaterThanX {
+    public static void main(String[] args) {
+
+    }
+    public static int greaterThanX(int arr[], int n, int x)
+    {
+        int cnt =0;
+        for(int i =0;i<n;i++){
+            if(arr[i]>x){
+                cnt++;
+            }
+        }
+        return cnt;
+        // Your code here
+    }
+}
